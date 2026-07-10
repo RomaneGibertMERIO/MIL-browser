06d90bd64ef231eac7ade71a3eaa2043e9703727
Romane GIBERT <romane.gibert@merio.fr>
2026-07-10 09:47:32 +0300

    Update preload.js
    ajout passerelle

--- electron-dist/preload.js
+++ electron-dist/preload.js
@@ -2,3 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 electron_1.contextBridge.exposeInMainWorld("electronAPI", {});
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    getBuiltinDatabase: () => ipcRenderer.invoke('get-builtin-database')
+});
