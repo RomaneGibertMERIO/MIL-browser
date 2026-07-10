c2cc08610c208caf88a6affea54d6ff309f9b3d6
Romane GIBERT <romane.gibert@merio.fr>
2026-07-10 10:25:06 +0300

    Update preload.js
    mauvais nom

--- electron-dist/preload.js
+++ electron-dist/preload.js
@@ -5,6 +5,7 @@ electron_1.contextBridge.exposeInMainWorld("electronAPI", {});
 
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
+// On utilise un nom unique pour éviter le conflit sur le 'window'
+contextBridge.exposeInMainWorld('milBrowserAPI', {
     getBuiltinDatabase: () => ipcRenderer.invoke('get-builtin-database')
 });
