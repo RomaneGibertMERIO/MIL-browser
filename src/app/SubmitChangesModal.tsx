3935c5bd652baece79973145cf1000d9b4c5eff7
Romane GIBERT <romane.gibert@merio.fr>
2026-07-14 10:56:20 +0300

    Update SubmitChangesModal.tsx

--- src/app/SubmitChangesModal.tsx
+++ src/app/SubmitChangesModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAppStore, type MockChangeItem } from "../store/appStore";
+import { useAppStore } from "../store/appStore";
 
 interface SubmitChangesModalProps {
   onClose: () => void;
