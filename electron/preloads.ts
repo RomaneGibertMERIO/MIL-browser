c9dffb17ff7f03e8f51abbe7759d182653685f9a
Romane GIBERT <romane.gibert@merio.fr>
2026-07-15 09:51:23 +0300

    Update preloads.ts

--- electron/preloads.ts
+++ electron/preloads.ts
@@ -2,4 +2,10 @@ import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("electronAPI", {
   getSystemUsername: () => ipcRenderer.invoke("get-system-username"),
+  
+  // Pipeline Git locale
+  gitSetRepoPath: (path: string) => ipcRenderer.invoke("git:set-path", path),
+  gitSync: (username: string) => ipcRenderer.invoke("git:sync", username),
+  gitSubmitProfile: (profile: any) => ipcRenderer.invoke("git:submit-profile", profile),
+  gitApproveProfile: (profileId: string) => ipcRenderer.invoke("git:approve-profile", profileId),
 });
